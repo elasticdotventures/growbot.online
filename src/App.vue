d7f7222235efdabb883a8f9a5f2a9ead54f95709
Brianh sm311s11k3s0ld3r <b+sm3lly@elastic.ventures>
2021-06-20 15:30:21 +1000

    test

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!--
+    <img alt="Vue logo" src="./assets/logo.png" />
+  -->
+  <HelloWorld msg="info@growbot.online" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <img src="GROWPOTBOT-L1.0b7c3f71.png" />
+  <!--
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  -->
+</template>
+
+<script>
+export default {
+  name: 'GrowPotBot',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
